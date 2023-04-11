f37cf9c6fc55963dcc3dc118499c6ff52f780025
Alexandre Vieira <alexandre.vieira@microsoft.com>
2023-04-11 17:27:31 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const Joi = require('joi');
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+  res.send(`
+    <form method="post" action="/validate">
+      <label for="fiscalNumber">Fiscal Number:</label>
+      <input type="text" id="fiscalNumber" name="fiscalNumber" required>
+      <button type="submit">Validate</button>
+    </form>
+  `);
+});
+
+app.post('/validate', (req, res) => {
+  const schema = Joi.object({
+    fiscalNumber: Joi.string().required(),
+  });
+
+  const { error } = schema.validate(req.body);
+
+  if (error) {
+    res.status(400).send(error.details[0].message);
+    return;
+  }
+
+  const fiscalNumber = req.body.fiscalNumber;
+
+  if (isValidFiscalNumber(fiscalNumber)) {
+    res.send('Valid fiscal number!');
+  } else {
+    res.send('Invalid fiscal number.');
+  }
+});
+
+function isValidFiscalNumber(fiscalNumber) {
+    const fiscalNumberRegex = /^[0-9]{2}[0,1][0-9][0-3][0-9][0-9]{4}$/;
+    
+    if (fiscalNumberRegex.test(fiscalNumber)) {
+        return true;
+    } else {
+        return false;
+    }
+
+
+}
+
+app.listen(3000, () => {
+  console.log('Server started on port 3000');
+});
